7208f00ea7cc5b2efda84e08ed1f278f91ee1630
jonathansevilla <jonathansevilla.7@gmail.com>
2023-03-16 18:45:44 -0600

    Finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -65,13 +65,12 @@ def endrow():
 
 @register.simple_tag
 def col(extra_classes=""):
-    return format_html('<div class="row {}">', extra_classes)
+    return format_html('<div class="col {}">', extra_classes)
 
 
 @register.simple_tag
 def endcol():
     return format_html("</div>")
-
 
 
 @register.simple_tag(takes_context=True)
